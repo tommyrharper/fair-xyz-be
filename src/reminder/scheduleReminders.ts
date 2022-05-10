581076b1766ca0b33e5ebdde656f0c3916a4c1ec
tommyrharper <thomasrobertharper@gmail.com>
2022-05-10 20:51:30 +0100

    Refactor scheduleReminders to use loop

--- src/reminder/scheduleReminders.ts
+++ src/reminder/scheduleReminders.ts
@@ -5,24 +5,21 @@ import {
   sendReminderEmail,
 } from './utils';
 
-const scheduleReminders = (
+export const scheduleReminders = (
   launchDate: Date,
   collectionName: string,
   email: string,
 ) => {
   const emailStrings = getEmailStrings(collectionName);
   const reminderTimes = getReminderTimes(launchDate);
-  const job1 = scheduleJob(reminderTimes[0], () => {
-    sendReminderEmail(email, emailStrings[0]);
+  const jobs = [];
+
+  reminderTimes.forEach((reminderTime, i) => {
+    const emailString = emailStrings[i];
+    const sendEmail = () => sendReminderEmail(email, emailString);
+    const job = scheduleJob(reminderTime, sendEmail);
+    jobs.push(job);
   });
-  const job2 = scheduleJob(reminderTimes[1], () => {
-    sendReminderEmail(email, emailStrings[1]);
-  });
-  const job3 = scheduleJob(reminderTimes[2], () => {
-    sendReminderEmail(email, emailStrings[2]);
-  });
-  const job4 = scheduleJob(reminderTimes[3], () => {
-    sendReminderEmail(email, emailStrings[3]);
-  });
-  return [job1, job2, job3, job4];
+
+  return jobs;
 };
