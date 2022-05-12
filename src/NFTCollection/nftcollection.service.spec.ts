884ca7c4590d7f33aa8068974a85ddfebf9e9419
tommyrharper <thomasrobertharper@gmail.com>
2022-05-12 22:14:11 +0100

    fix(test): updateNFTCollection updates email reminder jobs

--- src/NFTCollection/nftcollection.service.spec.ts
+++ src/NFTCollection/nftcollection.service.spec.ts
@@ -28,6 +28,8 @@ import { NFTCollectionService } from './nftcollection.service';
 import { addDays } from 'date-fns';
 import { Reminder } from '../reminder/reminder.entity';
 // import waitForExpect from 'wait-for-expect';
+// await waitForExpect(() => {
+// });
 
 const NEW_COLLECTION_NAME = 'New name';
 
@@ -98,12 +100,6 @@ describe('NFTCollectionService', () => {
 
   it('updateNFTCollection updates email reminder jobs', async () => {
     const addEmailJobsSpy = jest.spyOn(emailQueue, 'addBulk');
-    // addEmailJobsSpy.mockImplementation((jobs: { name?: string; data: any; opts?: Omit<JobOptions, "repeat">; }[]) => {
-    //   return addEmailJobsSpy(jobs)
-    //   return Promise.resolve([{}] as Job<any>[]);
-    // });
-
-    // (jobs: { name?: string; data: any; opts?: Omit<JobOptions, "repeat">; }[]) => Promise<Job<any>[]>
 
     const collection = await getCollectionByName(em, COLLECTION_NAME);
 
@@ -124,44 +120,47 @@ describe('NFTCollectionService', () => {
       undefined,
       true,
     );
-    // await waitForExpect(() => {
-    // });
+
     expect(addEmailJobsSpy).toHaveBeenCalledTimes(2);
 
-    // const reminder1Jobs: Job[] = await addEmailJobsSpy.mock.results[0].value;
-    // const reminder2Jobs: Job[] = await addEmailJobsSpy.mock.results[1].value;
-    // const jobsMatrix = [reminder1Jobs, reminder2Jobs];
+    const reminder1Jobs: Job[] = await addEmailJobsSpy.mock.results[0].value;
+    const reminder2Jobs: Job[] = await addEmailJobsSpy.mock.results[1].value;
+    const jobsMatrix = [reminder1Jobs, reminder2Jobs];
 
-    // jobsMatrix.forEach((jobs) => {
-    //   expect(jobs.length).toBe(4);
+    const expectedEmails = [TEST_EMAIL, TEST_EMAIL_TWO];
 
-    //   const expectedEmailContent = [
-    //     'REMINDER - THE COLLECTION NEW NAME LAUNCHES IN 1 DAY',
-    //     'REMINDER - THE COLLECTION NEW NAME LAUNCHES IN 1 HOUR',
-    //     'REMINDER - THE COLLECTION NEW NAME LAUNCHES IN 30 MINS',
-    //     'NEW NAME IS LAUNCHING NOW!',
-    //   ];
+    jobsMatrix.forEach((jobs, j) => {
+      expect(jobs.length).toBe(4);
 
-    //   const launchTime = new Date(collection.launchDate).getTime();
-    //   const expectedDelayLength = [
-    //     launchTime - reminderUpdatedTime - ONE_DAY_IN_MILLISECONDS,
-    //     launchTime - reminderUpdatedTime - ONE_HOUR_IN_MILLISECONDS,
-    //     launchTime - reminderUpdatedTime - HALF_HOUR_IN_MILLISECONDS,
-    //     launchTime - reminderUpdatedTime - 0,
-    //   ];
+      const expectedEmail = expectedEmails[j];
 
-    //   jobs.forEach((job, i) => {
-    //     expect(job.id).toBe(`${collection.uuid}-${TEST_EMAIL}-${i}`);
-    //     expect(job.data.email).toBe(TEST_EMAIL);
-    //     expect(job.data.text).toBe(expectedEmailContent[i]);
+      const expectedEmailContent = [
+        'REMINDER - THE COLLECTION NEW NAME LAUNCHES IN 1 DAY',
+        'REMINDER - THE COLLECTION NEW NAME LAUNCHES IN 1 HOUR',
+        'REMINDER - THE COLLECTION NEW NAME LAUNCHES IN 30 MINS',
+        'NEW NAME IS LAUNCHING NOW!',
+      ];
 
-    //     const timingError = Math.abs(expectedDelayLength[i] - job.opts.delay);
+      const launchTime = new Date(collection.launchDate).getTime();
+      const expectedDelayLength = [
+        launchTime - reminderUpdatedTime - ONE_DAY_IN_MILLISECONDS,
+        launchTime - reminderUpdatedTime - ONE_HOUR_IN_MILLISECONDS,
+        launchTime - reminderUpdatedTime - HALF_HOUR_IN_MILLISECONDS,
+        launchTime - reminderUpdatedTime - 0,
+      ];
 
-    //     // This tests that each email with be sent within 250ms of the expected time frame
-    //     expect(timingError).toBeLessThan(QUARTER_OF_A_SECOND);
+      jobs.forEach((job, i) => {
+        expect(job.id).toBe(`${collection.uuid}-${expectedEmail}-${i}`);
+        expect(job.data.email).toBe(expectedEmail);
+        expect(job.data.text).toBe(expectedEmailContent[i]);
 
-    //     job.remove();
-    //   });
-    // });
+        const timingError = Math.abs(expectedDelayLength[i] - job.opts.delay);
+
+        // This tests that each email with be sent within 250ms of the expected time frame
+        expect(timingError).toBeLessThan(QUARTER_OF_A_SECOND);
+
+        job.remove();
+      });
+    });
   });
 });
