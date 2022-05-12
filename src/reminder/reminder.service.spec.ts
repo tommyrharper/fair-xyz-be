42bfb26a302e96dc8aaa782796c84f194c63e9f4
tommyrharper <thomasrobertharper@gmail.com>
2022-05-12 20:13:08 +0100

    test: check delay time is accurate for each email

--- src/reminder/reminder.service.spec.ts
+++ src/reminder/reminder.service.spec.ts
@@ -22,6 +22,10 @@ import { GRAPHQL_CONFIG } from '../app.module';
 import { Job } from 'bull';
 
 const TEST_EMAIL = 'example@gmail.com';
+const ONE_DAY_IN_MILLISECONDS = 86_400_000;
+const ONE_HOUR_IN_MILLISECONDS = 3_600_000;
+const HALF_HOUR_IN_MILLISECONDS = 1_800_000;
+const QUARTER_OF_A_SECOND = 250;
 
 const INITIAL_MIGRATION = 'Migration20220509215402';
 
@@ -100,6 +104,7 @@ describe('ReminderService', () => {
       name: collectionName,
     });
 
+    const reminderCreatedTime = new Date().getTime();
     await service.createReminder(TEST_EMAIL, collection.uuid);
 
     expect(addBulkSpy).toHaveBeenCalledTimes(1);
@@ -113,10 +118,23 @@ describe('ReminderService', () => {
       'BEAUTY EMBODIED IS LAUNCHING NOW!',
     ];
 
+    const launchTime = new Date(collection.launchDate).getTime();
+    const expectedDelayLength = [
+      launchTime - reminderCreatedTime - ONE_DAY_IN_MILLISECONDS,
+      launchTime - reminderCreatedTime - ONE_HOUR_IN_MILLISECONDS,
+      launchTime - reminderCreatedTime - HALF_HOUR_IN_MILLISECONDS,
+      launchTime - reminderCreatedTime - 0,
+    ];
+
     jobs.forEach((job, i) => {
       expect(job.id).toBe(`${collection.uuid}-${TEST_EMAIL}-${i}`);
       expect(job.data.email).toBe(TEST_EMAIL);
       expect(job.data.text).toBe(expectedEmailContent[i]);
+
+      const timingError = Math.abs(expectedDelayLength[i] - job.opts.delay);
+
+      // This tests that each email with be sent within 250ms of the expected time frame
+      expect(timingError).toBeLessThan(QUARTER_OF_A_SECOND);
 
       job.remove();
     });
